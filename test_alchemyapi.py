c99721d318a31055b3b14a599d68022680de75e8
Lan Yang <ireneyanglan@gmail.com>
2015-04-15 21:58:58 -0400

    m

--- test_alchemyapi.py
+++
@@ -1,5 +0,0 @@
-from alchemyapi import AlchemyAPI
-alchemyapi = AlchemyAPI()
-myText = "I love to get started with AlchemyAPI!"
-response = alchemyapi.sentiment("text", myText)
-print "Sentiment: ", response["docSentiment"]["type"]
